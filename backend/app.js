672fde5e206c78696a7caca9af44a476a540afa9
Oscar Treviño <otrevinog29@gmail.com>
2025-05-26 12:15:07 -0600

    init

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+// Import Express
+const express = require('express');
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express.js Tutorial');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
